b9492b01fa114d2dba28b4cfd4d439e21d6a5911
somename <someemail>
2015-10-14 16:28:49 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/BowlingGame.java
+++ src/org/unioulu/tol/sqat/bsc/BowlingGame.java
@@ -4,23 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BowlingGame {
+	public static int Frameturn = 0;
 	//a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
-	
-	public BowlingGame(){}
+	public static void main(String arg[]){
+		BowlingGame bg=new BowlingGame();
+		Frame fe=new Frame(0, 0);
+		
+		bg.addFrame(null);//ready to start
+	}
+	public BowlingGame(){
+		
+	}
 	
 	public void addFrame(Frame frame){
 		//to be implemented
 	}
 	
 	public void setBonus(int firstThrow, int secondThrow) {
-		//to be implemented
+		if (!isNextFrameBonus()){
+			
+		}
 	}
 	
 	public int score(){
 		//to be implemented
-		return 0;
+		return score();
 	}
 	
 	public boolean isNextFrameBonus(){
--- src/org/unioulu/tol/sqat/bsc/Frame.java
+++ src/org/unioulu/tol/sqat/bsc/Frame.java
@@ -1,6 +1,7 @@
 package org.unioulu.tol.sqat.bsc;
 
 public class Frame {
+	
 	private int firstThrow;
 	private int secondThrow;
 	
@@ -10,32 +11,43 @@ public class Frame {
 	}
 	
 	//the score of a single frame
-	public int score(){
-		//to be implemented
-		return 0;
+	public int score(int j){
+		int roll=0;int score=0;
+		for(int i=0;i<j;i++){
+			if(isStrike()){
+				score+=10+bonus(roll);
+				roll++;
+			}
+			else if(isSpare()){
+				score+=10+bonus(roll);
+				roll++;
+			}
+		
+		}
+		return score;
 	}
 
 	//returns whether the frame is a strike or not
 	public boolean isStrike(){
 		//to be implemented
-		return false;
+		return(firstThrow==10);
 	}
 	
 	//return whether a frame is a spare or not
 	public boolean isSpare(){
 		//to be implemented
-		return false;
+		return (firstThrow+secondThrow==10);
 	}
 
 	//return whether this is the last frame of the match
 	public boolean isLastFrame(){
-		//to be implemented
+		//to be implementedrolle 	//return (BowligGame.Frameturnn==10);
 		return false;
 	}
 
 	//bonus throws
-	public int bonus(){
-		//to be implemented
+	public int bonus(int m){
+
 		return 0;
 	}
 }
